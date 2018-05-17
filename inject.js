08b96630805e07d348fa8c33c388b7a4fcf57a6d
Wasson <dwssn@amazon.com>
2018-05-16 21:14:20 -0400

    version 1.3 checks emp.co.uk for stock

--- inject.js
+++ inject.js
@@ -20,5 +20,21 @@
 		    }
 		} while (m);
 		document.getElementsByClassName('h-margin-b-tight styles__ShippingHeading-s1n8m629-1 pOnoh')[0].innerHTML = text + document.getElementsByClassName('h-margin-b-tight styles__ShippingHeading-s1n8m629-1 pOnoh')[0].innerHTML
+	} else if (document.location.href.indexOf('emp.co.uk') > -1) {
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.open( "GET", document.location.href, false );
+		xmlHttp.send( null );
+		var text = ""
+		var html = xmlHttp.responseText;
+		var re = /data-available="(.*?)"/g;
+		var m;
+		do {
+		    m = re.exec(html);
+		    if (m) {
+		        text = text + m[1] + " available online.</br>";
+		    }
+		} while (m);
+		document.getElementsByClassName('product-variations')[0].style.fontSize = '18px';
+		document.getElementsByClassName('product-variations')[0].innerHTML = text;
 	}
 })();

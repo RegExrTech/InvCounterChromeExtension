47fe864eed6733492b1cb5977493773a8aa2b5a4
Devon Wasson <devonwasson@gmail.com>
2018-06-20 10:47:54 -0400

    added support for funko-shop

--- inject.js
+++ inject.js
@@ -36,5 +36,12 @@
 		} while (m);
 		document.getElementsByClassName('product-variations')[0].style.fontSize = '18px';
 		document.getElementsByClassName('product-variations')[0].innerHTML = text;
+	} else if (document.location.href.indexOf('funko-shop') > -1) {
+		document.getElementById('quantity').type = "visible";
+		document.getElementById('quantity').value = 999999999;
+		document.getElementsByClassName('input-row')[2].children[0].click();
+		// document.getElementsByClassName('simple-popup')[0].innerHTML.split(" ")[4];
+		document.getElementById('quantity').value = 1;
+
 	}
 })();
